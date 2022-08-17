49931542fa17f49d06a4a1c59215a25ca01b5871
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-17 15:49:35 +0200

    finally

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: %i[create index new show] do
+    resources :reviews, only: %i[create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "La Loo", address: "Copenhagen", category: "italian")
+Restaurant.create(name: "Leppo", address: "Stockholm", category: "japanese")
+Restaurant.create(name: "Moron", address: "Stockholm", category: "belgian")
+Restaurant.create(name: "Leppo", address: "Reykjavik", category: "french")
+Restaurant.create(name: "Leppo", address: "Oslo", category: "chinese")
